b83ebd4b4351bffe12f028432e328541d2a3980b
shaobo99 <shaobo7799@163.com>
2024-03-18 22:03:34 +0800

    测试打包4

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "hello word"
-docker  --verison
+echo "开始编译docker 镜像"
+#docker  --verison
+docker build -f docker/Dockerfile -t testdemo:v1.0 .
+docker run -d -p 8888:8888 --name xiaobo-app testdemo:v1.0
