5e7413358019a9b438c03eda54b0114040ca9e91
shaobo99 <shaobo7799@163.com>
2024-03-27 15:39:54 +0800

    hadoop测试2

---
+++ deploy/hadoop/conf_dir/env.sh
@@ -0,0 +1,6 @@
+export JAVA_HOME=/opt/jdk
+export JRE_HOME=$JAVA_HOME/jre
+export CLASSPATH=.:$JAVA_HOME/lib
+export PATH=$PATH:$JAVA_HOME/bin
+export HADOOP_HOME=/opt/hadoop
+export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
